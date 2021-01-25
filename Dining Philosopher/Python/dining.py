8c7ab26df2dec8177ddcadb77340e4c73c61d23f
rahulrmsh <rahulramesh13.rr@gmail.com>
2021-01-25 22:24:33 +0530

    code update

--- Dining Philosopher/Python/dining.py
+++ Dining Philosopher/Python/dining.py
@@ -1,29 +1,30 @@
 # Dining Philosopher - Python
 
 n = 0
-state = []
+arr = []
 class Condition:
     Thinking = 0
     Eating = 1
     Hungry = 2
 
 def eat(x):
-    state[x] = Condition.Hungry;
-    if(state[(x + 4) % 5] == Condition.Thinking and state[(x - 1) % 5] == Condition.Thinking and state[x] == Condition.Hungry):
-        state[x] = Condition.Eating;
+    arr[x] = Condition.Hungry;
+    if(arr[(x + 4) % n] == Condition.Thinking and arr[(x - 1) % n] == Condition.Thinking and arr[x] == Condition.Hungry):
+        arr[x] = Condition.Eating;
         print("| {} is now eating.\n".format(x));
     else:
         print("| {} should wait\n".format(x));
     
 def think(x):
-    state[x] = Condition.Thinking;
+    arr[x] = Condition.Thinking;
     eat((x-1)%5);
     eat((x+4)%5);
     print("| {} stopped eating.".format(x));
 
 int(input("Enter number of philosophers: "))
 for i in range(n):
-    state.append(Condition.Thinking)
+    arr.append(0)
+print(arr)
 while(1):
     res = int(input("| Choose from below : \n|1. Start Eating\n|2. Stop Eating\n|3. Exit\n| Enter Response : "))
     if res == 3:
