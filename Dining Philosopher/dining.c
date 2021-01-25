fb22c32d2ff92c529fe11323147b7ea53816857d
rahulrmsh <rahulramesh13.rr@gmail.com>
2021-01-25 20:58:37 +0530

    code update

--- Dining Philosopher/dining.c
+++ Dining Philosopher/dining.c
@@ -11,10 +11,15 @@ enum {
     EATING
 } state[5];
 
+int i, j, k, flag[5], n;
 void initialize(){
-
+    for(i = 0; i < n; i++){
+        state[i] = THINKING;
+        flag[i] = 0;
+    }
 }
 int main(){
+    n = 5;
     initialize();
     return 0;
 }
