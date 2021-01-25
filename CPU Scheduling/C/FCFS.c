b5c676758ed6f37f96ef1a79256ac1a824535238
rahulrmsh <rahulramesh13.rr@gmail.com>
2021-01-25 22:42:35 +0530

    code update

--- CPU Scheduling/C/FCFS.c
+++ CPU Scheduling/C/FCFS.c
@@ -2,10 +2,15 @@
 
 #include <stdio.h>
 int main(){
-    int n, i, k;
+    int n, i, j;
     scanf("Enter the number of processes  : %d",&n);
-    int burst[n];
+    int burst[n], average[n], turn[n];
     for(i = 0; i < n; i++){
         scanf("Enter the burst time of processes : %d",&burst[i]);
+    }
+    for(i = 0; i < n; i++){
+        for(j = 0; j < n; j++){
+
+        }
     }
 }
