b16e6b1b3b0943c3c71f150b4dc70b6b33711741
rahulrmsh <rahulramesh13.rr@gmail.com>
2021-01-25 23:34:54 +0530

    code update

--- Producer - Consumer/pc.c
+++ Producer - Consumer/pc.c
@@ -1,1 +1,22 @@
 // Producer-Consumer - C Language
+#include<stdlib.h>
+#include<stdio.h>
+#include<ctype.h>
+int x = 0, y = 1, s = 1, empty = 4, full = 0, op;
+int wait(int a){
+    while(a <= 0){
+        ;
+    }
+    return (--a);
+}
+int signal(int a){
+    return (++a);
+}
+void produce(){
+    empty = wait(empty);
+    s = wait(s);
+    x = (x+1)%5;
+    
+}
+int main(){
+}
