073decc3f51667a6581c6299577fe41ace25727a
rahulrmsh <rahulramesh13.rr@gmail.com>
2021-01-25 22:38:08 +0530

    code update

--- CPU Scheduling/C/FCFS.c
+++ CPU Scheduling/C/FCFS.c
@@ -1,3 +1,11 @@
 // First Come First Serve - C Language
 
 #include <stdio.h>
+int main(){
+    int n, i;
+    scanf("Enter the number of processes  : %d",&n);
+    int burst[n];
+    for(i = 0; i < n; i++){
+        scanf("Enter the burst time of processes %d : ",i+1);
+    }
+}
