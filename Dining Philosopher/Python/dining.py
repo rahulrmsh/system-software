2eec1bb8fe981902ace07aa257e408b2ac74358d
rahulrmsh <rahulramesh13.rr@gmail.com>
2021-01-25 22:15:18 +0530

    code update

--- Dining Philosopher/Python/dining.py
+++ Dining Philosopher/Python/dining.py
@@ -15,4 +15,5 @@ while(1):
         exit(0)
     elif res > 3:
         print("|\n| WRONG INPUT\n|\n");
-        continue();
+        continue;
+    id = int(input("| Choose from below : \n|1. Start Eating\n|2. Stop Eating\n|3. Exit\n| Enter Response : "))
