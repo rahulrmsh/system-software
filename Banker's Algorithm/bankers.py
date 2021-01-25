7bfb341ccdde2e13e643d16dc5126511e3c973a8
rahulrmsh <rahulramesh13.rr@gmail.com>
2021-01-25 16:12:40 +0530

    code update

---
+++ Banker's Algorithm/bankers.py
@@ -0,0 +1,15 @@
+# Banker's Algorithm - Python
+
+n = int(input("Enter number of processes: "))
+m = int(input("Enter number of resources: "))
+print("Enter resource per processes: \n")
+alloc = []
+maximum = []
+avail = []
+for i in range(n):
+    alloc.append([list(map(int, input("Enter allocated resource for process {} : ".format(i+1)).split()))])
+for i in range(n):
+    maximum.append([list(map(int, input("Enter maximum resource for process {} : ".format(i+1)).split()))])
+avail.append([list(map(int, input("Enter available resource : ").split()))])
+print(avail)
+print(*alloc)
