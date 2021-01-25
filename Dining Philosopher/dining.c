2b380dd2c916dd4cc7a86cb5d4040fbb80818117
rahulrmsh <rahulramesh13.rr@gmail.com>
2021-01-25 21:18:30 +0530

    code update

--- Dining Philosopher/dining.c
+++ Dining Philosopher/dining.c
@@ -11,19 +11,34 @@ enum {
     EATING
 } state[5];
 
-int i, j, k, flag[5], n;
+int i, flag[5], n, key;
 void initialize(){
     for(i = 0; i < 5; i++){
         state[i] = THINKING;
         flag[i] = 0;
     }
 }
+void eat(int x){
+    state[x] = HUNGRY;
+    if(state[x + 4 % 5] == THINKING && state[x - 1 % 5] == THINKING && state[x] == HUNGRY){
+        state[x] = EATING;
+        flag[x] = 1;
+        printf("| %d is now eating.",x );
+    }else{
+        printf("| %d should wait",x );
+    }
+}
 int main(){
     initialize();
     printf("| Choose from below : \n|1. Start Eating\n|2. Stop Eating\n|3. Exit\n| Enter Response : ");
     scanf("%d", &n);
+    printf("| Enter philosopher id : ");
+    scanf("%d", &key);
     if(n == 3){
-        
+        exit(0);
+    }
+    else if(n == 1){
+        eat(key);
     }
     return 0;
 }
