5cde0ad52b559df52d0192ed6f8b2320f98d88d0
rahulrmsh <rahulramesh13.rr@gmail.com>
2021-01-25 21:34:31 +0530

    code update

--- Dining Philosopher/dining.c
+++ Dining Philosopher/dining.c
@@ -20,34 +20,39 @@ void initialize(){
 }
 void eat(int x){
     state[x] = HUNGRY;
-    if(state[x + 4 % 5] == THINKING && state[x - 1 % 5] == THINKING && state[x] == HUNGRY){
+    if(state[(x + 4) % 5] == THINKING && state[(x - 1) % 5] == THINKING && state[x] == HUNGRY){
         state[x] = EATING;
         flag[x] = 1;
-        printf("| %d is now eating.",x );
+        printf("| %d is now eating.\n",x );
     }else{
-        printf("| %d should wait",x );
+        printf("| %d should wait\n",x );
     }
 }
 void think(int x){
     state[x] = THINKING;
     flag[x] = 0;
-    eat(x-1%5);
-    eat(x+4%5);
+    eat((x-1)%5);
+    eat((x+4)%5);
+    printf("| %d stopped eating.",x );
 }
 int main(){
     initialize();
+    printf("%d",state[0]);
     while(1){
         printf("| Choose from below : \n|1. Start Eating\n|2. Stop Eating\n|3. Exit\n| Enter Response : ");
         scanf("%d", &n);
-        printf("| Enter philosopher id : ");
-        scanf("%d", &key);
         if(n == 3){
             exit(0);
+        }else if (n > 3){
+            printf("|\n| WRONG INPUT\n|\n");
         }
-        else if(n == 1){
+        printf("| Enter philosopher id : ");
+        scanf("%d", &key);
+        if(n == 1){
+            printf("| %d", key );
             eat(key);
         }else if(n == 2){
-
+            think(key);
         }
     }
     return 0;
