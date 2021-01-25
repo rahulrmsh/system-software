1cc02ffcc07b5af2a3836b13a9ccce7234958fee
rahulrmsh <rahulramesh13.rr@gmail.com>
2021-01-25 21:06:45 +0530

    code update

--- Dining Philosopher/dining.c
+++ Dining Philosopher/dining.c
@@ -13,14 +13,17 @@ enum {
 
 int i, j, k, flag[5], n;
 void initialize(){
-    for(i = 0; i < n; i++){
+    for(i = 0; i < 5; i++){
         state[i] = THINKING;
         flag[i] = 0;
     }
 }
 int main(){
-    n = 5;
     initialize();
-    printf("| Choose from below : \n| Start Eating\n| Stop Eating\n| Exit\n| Enter Response : ");
+    printf("| Choose from below : \n|1. Start Eating\n|2. Stop Eating\n|3. Exit\n| Enter Response : ");
+    scanf("%d", &n);
+    if(n == 3){
+        
+    }
     return 0;
 }
