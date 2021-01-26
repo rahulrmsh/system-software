a79ad4fe3528b7c103539bbc57b8cd1ef57ef147
rahulrmsh <rahulramesh13.rr@gmail.com>
2021-01-26 16:55:07 +0530

    code update

--- Producer - Consumer/Python/pc.py
+++ Producer - Consumer/Python/pc.py
@@ -1,7 +1,5 @@
 # Producer-Consumer - Python
 
-x= 0, y = 1, s = 1, empty = 0, full = 10
-
 def wait(x):
     while(x<=0):
         pass
@@ -10,34 +8,39 @@ def wait(x):
 def signal(x):
     return ++x
 
-def produce():
-    global empty, s, x, full
+def produce(empty, s, x, full):
     empty = wait(empty)
     s = wait(s)
     x = (x + 1) % 5
     print("\nProducer produced item : {}\n".format(x))
     full = signal(full)
     s = signal(s)
+    return empty, s, x, full
 
-def consume():
-    global empty, s, y, full
+def consume(empty, s, y, full):
     full = wait(full)
     s = wait(s)
     print("\nConsumer consumed 1 item, total consumption : {}\n".format(y))
     y = (y + 1) % 5
     empty = signal(empty)
     s = signal(s)
+    return empty, s, x, full
 
+x = 0
+y = 1
+s = 1
+empty = 0
+full = 10
 while(True):
     print("\n1.Produce \n2.Consume \n3.Exit\n")
     op = int(input("\nEnter choice : "))
     if op == 1:
-        if s > 0 and empty != 0:
-            produce()
+        if s == 1 and empty != 0:
+            empty, s, x, full = produce(empty, s, x, full)
         else:
             print("\nBuffer is full\n")
     elif op == 2:
-        if s > 0 and full != 0:
-            consume()
+        if s == 1 and full != 0:
+            empty, s, x, full = consume(empty, s, x, full)
     elif op == 3:
         exit(0)
