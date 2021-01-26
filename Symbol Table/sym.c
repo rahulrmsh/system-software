ef70d367a097c1a8b6eb220ea1f3308f1edf9782
rahulrmsh <rahulramesh13.rr@gmail.com>
2021-01-26 18:21:18 +0530

    code update

--- Symbol Table/sym.c
+++ Symbol Table/sym.c
@@ -1,1 +1,14 @@
 // Symbol Table - C Language
+
+#include<stdlib.h>
+#include<string.h>
+#include<stdio.h>
+int max = 11, count = 0;
+int a[max];
+int main(){
+    int adr, res, i;
+    char label[20];
+    for(i=0;i<max;i++){
+        a[i] = 0
+    }
+}
