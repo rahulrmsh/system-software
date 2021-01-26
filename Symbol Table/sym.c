0a61e39446af2fee3048185c68efd33c3eee3b82
rahulrmsh <rahulramesh13.rr@gmail.com>
2021-01-26 18:23:49 +0530

    code update

--- Symbol Table/sym.c
+++ Symbol Table/sym.c
@@ -3,12 +3,21 @@
 #include<stdlib.h>
 #include<string.h>
 #include<stdio.h>
+struct Symbol{
+    int adr;
+    char label[20];
+}s[11];
 int max = 11, count = 0;
-int a[max];
+int a[11];
 int main(){
     int adr, res, i;
     char label[20];
     for(i=0;i<max;i++){
-        a[i] = 0
+        a[i] = 0;
+        s[i].adr = 0;
+        strcpy(s[i].label, "");
+    }
+    while(1){
+        
     }
 }
