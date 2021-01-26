e842cc9830d582f42edc0a32aeaeda00ca0155a7
rahulrmsh <rahulramesh13.rr@gmail.com>
2021-01-26 16:43:28 +0530

    code update

--- Producer - Consumer/Python/pc.py
+++ Producer - Consumer/Python/pc.py
@@ -1,6 +1,7 @@
 # Producer-Consumer - Python
 
 x= 0, y = 1, s = 1, empty = 0, full = 10
+
 def wait(x):
     while(x<=0):
         pass
@@ -9,6 +10,15 @@ def wait(x):
 def signal(x):
     return ++x
 
+def produce():
+    global empty, s
+    empty = wait(empty)
+    
+
+
 while(True):
     print("\n1.Produce \n2.Consume \n3.Exit\n")
     op = int(input("\nEnter choice : "))
+    if op == 1:
+        if s > 0 and empty != 0:
+            produce()
