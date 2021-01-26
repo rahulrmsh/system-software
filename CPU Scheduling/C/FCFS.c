7826f59d6ddd8f6991552546e29784728c04aa4a
rahulrmsh <rahulramesh13.rr@gmail.com>
2021-01-26 11:32:30 +0530

    code update

--- CPU Scheduling/C/FCFS.c
+++ CPU Scheduling/C/FCFS.c
@@ -1,20 +1,22 @@
 // First Come First Serve - C Language
 
 #include <stdio.h>
-int main(){
-    int n, i, j;
-    scanf("Enter the number of processes  : %d",&n);
-    int burst[n], average, turn[n];
-    for(i = 0; i < n; i++){
-        scanf("Enter the burst time of processes : %d",&burst[i]);
+#include <stdlib.h>
+int main() {
+  int n, i, j;
+  scanf("Enter the number of processes  : %d", &n);
+  int burst[n], average, turn[n];
+  for (i = 0; i < n; i++) {
+    scanf("Enter the burst time of processes : %d", &j);
+    burst[i] = j;
+  }
+  for (i = 0; i < n; i++) {
+    for (j = 0; j <= i; j++) {
+
+      turn[i] = turn[i] + burst[j];
     }
-    for(i = 0; i < n; i++){
-        for(j = 0; j < i; j++){
-            
-            turn[i] = turn[i] + burst[j];
-        }
-        average = average + burst[i];
-    }
-    printf("Average %d", average/n);
-    printf("Average %d", average/n);
+    average = average + burst[i];
+  }
+  printf("Average %d", average / n);
+  printf("Average %d", average / n);
 }
